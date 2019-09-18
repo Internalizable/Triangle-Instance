f3a38a887ad3ba71ec6bacb97980d991f524a692
Internalizable <elietgm1@gmail.com>
2019-09-18 11:45:20 +0300

    Add project files.

---
+++ Triangle Instance/Triangle.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+int getMaxTriangleHeight(int base);
+void buildTrianglePiece(int base, char usedChar, int maxHeight);
+
+int main() {
+
+	cout << "Please enter the width of the triangle's base (greater than one and odd)" << endl;
+
+	int baseLength{};
+	cin >> baseLength;
+
+	if (!cin.good()) {
+		cerr << "\aPlease type in a correct integer" << endl;
+		cerr << "Re-running the application" << endl;
+
+		main();
+
+		return 0;
+	}
+
+	if (baseLength <= 1 || (baseLength % 2) == 0) {
+		cerr << "\aPlease type in a correct positive odd integer" << endl;
+		cerr << "Re-running the application" << endl;
+
+		main();
+
+		return 0;
+	}
+
+	char characterUsed{};
+	cin >> characterUsed;
+
+	int maxHeight{ getMaxTriangleHeight(baseLength) };
+
+	buildTrianglePiece(baseLength, characterUsed, maxHeight);
+}
+
+int getMaxTriangleHeight(int base) {
+	int currentHeight{0};
+
+	for (int startingInt = 1; startingInt <= base; startingInt = startingInt + 2) {
+		++currentHeight;
+	}
+
+	return currentHeight;
+}
+
+void buildTrianglePiece(int base, char usedChar, int maxHeight) {
+	int currentHeight{ 0 };
+
+	for (int startingInt = 1; startingInt <= base; startingInt = startingInt + 2) {
+		++currentHeight;
+
+		if (startingInt == base) {
+			for (int i = 1; i <= base; ++i) {
+				cout << usedChar;
+			}
+		} else {
+			int getIndent{ maxHeight - currentHeight };
+
+			for (int i = 1; i <= getIndent; ++i) {
+				cout << " ";
+			}
+
+			for (int i = 1; i <= startingInt; ++i) {
+				cout << usedChar;
+
+				if (i == startingInt) {
+					cout << endl;
+				}
+			}
+		}
+
+	}
+	
+}
